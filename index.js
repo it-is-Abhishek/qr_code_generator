01b553d7398588805e1bf78f5f0617714889d12c
it-is-Abhishek <abhishekverma1489@gmail.com>
2025-09-26 09:26:38 +0530

    fixed bug for deployment

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 async function generateQR() {
     const text = document.getElementById("text").value;
-    const response = await fetch("http://127.0.0.1:5000/generate", {
+    const response = await fetch("https://web-production-0d56.up.railway.app/generate", {
         method : "POST",
         headers: {"Content-Type": "application/json"},
         body: JSON.stringify({ text: text })
