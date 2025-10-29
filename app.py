3dc3b55db2df5f2d9b5bf09690de85db1300b331
it-is-Abhishek <abhishekverma1489@gmail.com>
2025-10-29 12:14:26 +0530

    Add route for serving assets directory

--- app.py
+++ app.py
@@ -19,13 +19,17 @@ def css():
 def js():
     return send_from_directory('.', 'index.js')
 
+@app.route('/assets/<path:filename>')
+def assets(filename):
+    return send_from_directory('assets', filename)
+
 @app.route("/generate", methods = ["POST"])
 
 def generate_qr():
     data = request.json.get("text")
     if not data:
         return {"error": "no text provided"}, 400
-    
+
     qr = qrcode.QRCode(box_size = 10, border = 4)
     qr.add_data(data)
     qr.make(fit=True)
