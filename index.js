83a2b39c13fcb2e149db2e841bd81f5e3b29f920
it-is-Abhishek <abhishekverma1489@gmail.com>
2025-10-29 11:36:58 +0530

    Update JS to use relative URL for API calls

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 async function generateQR() {
-    const text = document.getElementById("text").value;
-    const response = await fetch("https://web-production-0d56.up.railway.app/generate", {
+const text = document.getElementById("text").value;
+    const response = await fetch("/generate", {
         method : "POST",
         headers: {"Content-Type": "application/json"},
         body: JSON.stringify({ text: text })
